ca5953106dc24d8b397ff07e5b6f762074ec9e10
Kateryna <kateryna.shevchenko.1@gmail.com>
2018-10-08 12:19:37 +0300

    add new

---
+++ js.js
@@ -0,0 +1,17 @@
+﻿
+  var get = function (id) {
+    return document.getElementById(id);
+  };
+  function login(){
+    var log_in=false;
+    var email=get("userEmail").value;
+    var password=get("userPas").value;
+      if(email=='Nikita1@gmail.com' && password=='NikitaSuperhero1'){
+        log_in=true;
+        document.location.href="login.html";
+      }
+      else{
+        alert("Invalid login or password!");
+      }
+    }
+  
